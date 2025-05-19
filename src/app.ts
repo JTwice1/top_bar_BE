3ec2c71da97cb40c64bd395a07a73f17dc13f4f0
JTwice1 <trnka.juraj8@gmail.com>
2025-05-19 16:28:48 +0200

    url for FE changed because slash

--- src/app.ts
+++ src/app.ts
@@ -90,7 +90,7 @@ if (process.env.NODE_ENV === 'development') {
 const corsOptions = {
   origin:
     process.env.NODE_ENV === 'production'
-      ? 'https://ruzovy-pasik.netlify.app/'
+      ? 'https://ruzovy-pasik.netlify.app'
       : 'http://localhost:5173',
   credentials: true,
 };
